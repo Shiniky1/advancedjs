f7779fe4177214cb3e46377a317137947f6871c0
Frontend JavaScript <frontendjavascript@iMAC-07.local>
2025-04-03 20:39:57 +0300

    curs2

--- curs1/Carte.js
+++ curs1/Carte.js
@@ -1,26 +1,25 @@
-class Carte{
-    constructor(titluCarte,autorCarte,edituraCarte,anCarte,pretCarte){
-        this.titlu=titluCarte;
-        this.autor=autorCarte;
-        this.editura=edituraCarte;
-        this.an=anCarte;
-        this.pret=pretCarte;
+class Carte {
+    constructor(titluCarte, autorCarte, edituraCarte, anCarte, pretCarte) {
+        this.titlu = titluCarte;
+        this.autor = autorCarte;
+        this.editura = edituraCarte;
+        this.an = anCarte;
+        this.pret = pretCarte;
     }
     obtineTitlu(){
         console.log(`Titlul cartii este ${this.titlu}`)
     }
     afiseazaInfo(){
-        console.log(`Detalii despre aceasta carte sunt:${this.titlu},${this.autor},${this.pret}`)
+        return `Detalii despre aceasta carte sunt: ${this.titlu}, ${this.autor}, ${this.pret}`;
     }
-    obtineInfo(){
-        console.log(`Detalii despre aceasta carte sunt: ${this.titlu},${this.autor},${this.pret}`)
-    }
-    deschidCarte(){
+    deschidCartea() {
         console.log('Am deschis cartea la pagina 15');
     }
-    randomIntFromInterval(min,max){
-        return Math.floor(Math.random()*(min-max+1)+min)
+    randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
+
+
 }
 
-export{Carte};
+export { Carte };
---
+++ curs2/modules/User.js
@@ -0,0 +1,34 @@
+class User{
+    #varsta;
+    _email;
+    constructor(id,nume){
+        //setam proprietatile
+        this.id=id;
+        this.nume=nume;
+    }
+
+    set email(myemail){
+        this._email=myemail;
+    }
+    get email(){
+        return this._email;
+    }
+
+    set varsta(age){
+        this.#varsta=age;
+    }
+    get varsta(){
+        return this.#varsta;
+    }
+
+    datePersonale(){
+        console.log(`Salut numele meu este ${this.nume}, adresa de mail este ${this._email}, varsta este ${this.#varsta}`);
+    }
+    mananca (claorii){
+        console.log(`Mananca ${claorii} pe zi.`);
+    }
+    doarme(ore){
+        console.log(`Doarme ${ore} pe zi`);
+    }
+}
+export {User}
