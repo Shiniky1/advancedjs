47bbfe534a6d062438fe65cd3eaa7d0a57946140
Frontend JavaScript <frontendjavascript@iMAC-07.local>
2025-05-07 20:41:49 +0300

    curs7

---
+++ curs6/ex3_1.js
@@ -0,0 +1,12 @@
+var StudentNou = /** @class */ (function () {
+    function StudentNou(code, name) {
+        this.studCode = code;
+        this.studName = name;
+    }
+    StudentNou.prototype.display = function () {
+        return "My unique code: ".concat(this.studCode, ", my name: ").concat(this.studName, ".");
+    };
+    return StudentNou;
+}());
+var studentnou = new StudentNou(1, "JoeRoot");
+console.log(studentnou.display());
